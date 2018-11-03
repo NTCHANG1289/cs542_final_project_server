0e48a66a7cddab650aba761b5fc1b16ae9c17931
Jonathan Wang <wangjonathan1992@gmail.com>
2018-11-03 17:04:52 -0400

    move dbconnection into separate file

--- index.js
+++ index.js
@@ -11,13 +11,5 @@ app.use(bodyParser.json({ type: '*/*' }));
 app.use(morgan('combined'));
 router(app);
 
-
-function getDBConnection() {
-    return new Client({
-        // connectionString: process.env.DATABASE_URL || "postgresql://localhost/test"
-        connectionString: process.env.DATABASE_URL || "postgresql://localhost/test"
-    });
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
