99c13943696eebef2b0c1b4faa7588dfcfded06a
alex <alexfuyang@gmail.com>
2018-11-06 18:54:03 -0500

    update db connection

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 module.exports = () => {
   return new Client({
-    //connectionString: process.env.DATABASE_URL || "postgresql://localhost/test"
-    connectionString: process.env.DATABASE_URL || "postgresql://alex:610041@localhost/test"
+    connectionString: process.env.DATABASE_URL || "postgresql://localhost/test"
+    //connectionString: process.env.DATABASE_URL || "postgresql://alex:610041@localhost/test"
   });
 }
